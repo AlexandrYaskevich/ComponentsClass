c2f38e004dcad6b18eaa9141e8eb50cfbcb42f74
AppVeyor <yask.alexandr@gmail.com>
2024-09-30 10:03:23 +0300

    eslint

--- src/components/ShopItemClass.jsx
+++ src/components/ShopItemClass.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+
 
 
 class ShopItemClass extends React.Component {
@@ -8,17 +8,17 @@ class ShopItemClass extends React.Component {
 
         return (
             <>
-                <div class="main-content">
+                <div className={"main-content"}>
                     <h2>{item.brand}</h2>
                     <h1>{item.title}</h1>
                     <h3>{item.description}</h3>
-                    <div class="description">
+                    <div className={"description"}>
                         {item.descriptionFull}
                     </div>
-                    <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
-                    <div class="divider"></div>
-                    <div class="purchase-info">
-                        <div class="price">{item.currency}{item.price}</div>
+                    <div className={"highlight-window mobile"}><div className={"highlight-overlay"}></div></div>
+                    <div className={"divider"}></div>
+                    <div className={"purchase-info"}>
+                        <div className={"price"}>{item.currency}{item.price}</div>
                         <button>Добавить в корзину</button>
                     </div>
                 </div>
